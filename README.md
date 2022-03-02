af0affd3d48d79c63b27dd7a2fcba4c1859e77f2
MVG_Sarv <lucas@madvikinggames.com>
2022-03-02 17:16:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MVGPrototipo
+
+Developed with Unreal Engine 4
---
+++ Source/MVGPrototipo/Private/TestScript.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestScript.h"
+
+TestScript::TestScript()
+{
+}
+
+TestScript::~TestScript()
+{
+}
