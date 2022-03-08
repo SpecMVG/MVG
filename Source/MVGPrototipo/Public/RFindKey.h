d0a32b89d3e73c19aa7b29d612ff10919720d6f6
SpecMVG <100797515+SpecMVG@users.noreply.github.com>
2022-03-08 15:16:06 -0300

    f

---
+++ Source/MVGPrototipo/Public/RFindKey.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "RFindKey.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MVGPROTOTIPO_API URFindKey : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
